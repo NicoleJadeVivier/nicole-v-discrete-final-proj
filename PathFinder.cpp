bbd60b59cf2e6dcf077ceef45def59bc80e80570
NicoleJadeVivier <nvivier@mail.smu.edu>
2021-11-22 20:37:45 -0600

    Adding pathFinder class

---
+++ PathFinder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicole Vivier on 11/22/2021.
+//
+
+#include "PathFinder.h"
---
+++ PathFinder.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicole Vivier on 11/22/2021.
+//
+
+#ifndef INC_21F_FLIGHT_PLANNER_PATHFINDER_H
+#define INC_21F_FLIGHT_PLANNER_PATHFINDER_H
+
+
+class PathFinder {
+
+};
+
+
+#endif //INC_21F_FLIGHT_PLANNER_PATHFINDER_H
