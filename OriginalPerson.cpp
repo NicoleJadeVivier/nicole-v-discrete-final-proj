2c438c8159ef9b6a6587b3b5f86457b273e4b863
NicoleJadeVivier <nvivier@mail.smu.edu>
2021-11-22 20:36:29 -0600

    Implemented OriginalPersons class

--- OriginalPerson.cpp
+++ OriginalPerson.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "OriginalPerson.h"
+
+OriginalPerson::OriginalPerson() {
+    name = " ";
+}
+
+OriginalPerson::OriginalPerson(string theName) {
+    name = theName;
+}
+
+OriginalPerson::OriginalPerson(string theName, ContactedPerson person) {
+    name = theName;
+    contactsList.append(person);
+}
+
+bool OriginalPerson::operator==(const OriginalPerson &person) const {
+    if (name == person.name)
+        return true;
+    else
+        return false;
+}
+
+string OriginalPerson::getOGName() {
+    return name;
+}
+
+std::ostream &operator<<(ostream &os, const OriginalPerson &person) {
+    os << person.name;
+    return os;
+}
+
+void OriginalPerson::addCity(ContactedPerson person) {
+    contactsList.append(person);
+}
+
+void OriginalPerson::printInfo() {
+    cout << "Original Person: " << name << endl;
+    cout << "Contacts: " << endl;
+    contactsList.set_front();
+    while (!contactsList.currAtNull()) {
+        cout << "\t" << contactsList.get_curr_value();
+        contactsList.next();
+    }
+}
+
+void OriginalPerson::printContacts() {
+    contactsList.print();
+}
+
+void OriginalPerson::reset() {
+    contactsList.set_front();
+}
+
+void OriginalPerson::moveIter() {
+    if (!contactsList.currAtNull()) {
+        contactsList.next();
+    } else
+        throw std::runtime_error("OriginalPerson moveIter(): already at nullptr");
+}
+
+ContactedPerson &OriginalPerson::getPerson() {
+    if (!contactsList.currAtNull())
+        return contactsList.get_curr_value();
+    else
+        throw std::runtime_error("Original Person getPerson(): cannot return nullptr");
+}
+
+DSLinkedList<ContactedPerson> &OriginalPerson::getContactsList() {
+    return contactsList;
+}
+
+
--- OriginalPerson.h
+++ OriginalPerson.h
@@ -4,11 +4,33 @@
 
 #ifndef INC_21F_FLIGHT_PLANNER_ORIGINALPERSON_H
 #define INC_21F_FLIGHT_PLANNER_ORIGINALPERSON_H
-#include <string>
+#include "ContactedPerson.h"
+#include "DSLinkedList.h"
 
 class OriginalPerson {
 private:
-    string name
+    string name;
+    DSLinkedList<ContactedPerson> contactsList;
+public:
+    OriginalPerson();
+    OriginalPerson(string);
+    OriginalPerson(string, ContactedPerson);
+
+    bool operator== (const OriginalPerson&)const;
+    string getOGName();
+
+    friend std::ostream& operator<< (std::ostream&, const OriginalPerson&);
+
+    void addCity(ContactedPerson);
+    void printInfo();
+    void printContacts();
+
+    //iterator wrapper functions to make it easier to implement in PathFinder
+    void reset();
+    void moveIter();
+    ContactedPerson& getPerson();
+    DSLinkedList<ContactedPerson>& getContactsList();
+
 };
 
 
