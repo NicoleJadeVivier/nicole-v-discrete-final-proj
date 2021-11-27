bb7da9ff5746c595362486d5d4f5a158fdb6c130
NicoleJadeVivier <nvivier@mail.smu.edu>
2021-11-26 20:45:30 -0600

    Adding the paths class

---
+++ Paths.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicole Vivier on 11/26/2021.
+//
+
+#include "Paths.h"
---
+++ Paths.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicole Vivier on 11/26/2021.
+//
+
+#ifndef INC_21F_FLIGHT_PLANNER_PATHS_H
+#define INC_21F_FLIGHT_PLANNER_PATHS_H
+
+
+class Paths {
+
+};
+
+
+#endif //INC_21F_FLIGHT_PLANNER_PATHS_H
